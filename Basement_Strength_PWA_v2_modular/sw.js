62974373e6b6f7b3a4736a7a3862088911b6021a
chasingmorechickadees <alicia.arcidiacono@gmail.com>
2026-07-27 12:53:47 -0700

    Delete Basement_Strength_PWA_v2_modular directory

--- Basement_Strength_PWA_v2_modular/sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE="basement-strength-v2";
-const ASSETS=["./","./index.html","./css/app.css","./js/app.js","./js/config.js","./js/db.js","./js/workouts.js","./js/coach.js","./js/progression.js","./js/backup.js","./js/ui.js","./data/workouts.json","./data/default-settings.json","./manifest.webmanifest","./icons/icon-192.png","./icons/icon-512.png"];
-self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
-self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
-self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(cache=>cache.put(e.request,copy));return r}).catch(()=>caches.match("./index.html"))))});
